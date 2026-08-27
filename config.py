7ad210d005d77787b045bde0690fa1ac9a83fe43
Francisco del Villar <delvillarfr@gmail.com>
2026-08-27 16:41:00 -0600

    Write agent's identity and a format for flight itineraries.

---
+++ config.py
@@ -0,0 +1,32 @@
+IDENTITY = """
+You are a customer care representative for Northwest Airlines.
+You are addressing a customer whose flight is overbooked.
+Your role is to guide the customer through Northwest's traveler assurance
+program, which pays customers to accept a later flight.
+"""
+
+ITINERARIES = """
+current itinerary:
+  aircraft: 737 MAX 8
+  number: TW 3742
+  departure: Denver International (DEN), concourse C, gate TBD, 13:25 local (tomorrow)
+  arrival: Bozeman Yellowstone International (BZN), 15:09 local (tomorrow)
+  seat: 25B
+  attributes: middle seat in the back of the plane.
+
+alternative itinerary 1:
+  aircraft: 737 MAX 8
+  number: TW 4476
+  departure: Denver International (DEN), concourse C, gate TBD, 17:05 local (tomorrow)
+  arrival: Bozeman Yellowstone International (BZN), 18:49 local (tomorrow)
+  seat: 12A
+  attributes: west-facing window before the wing with great views of the Wind River Range 45-55 minutes into the flight under the evening sun.
+
+alternative itinerary 2:
+  aircraft: 737 MAX 8
+  number: TW 4476
+  departure: Denver International (DEN), concourse C, gate TBD, 17:05 local (today)
+  arrival: Bozeman Yellowstone International (BZN), 18:49 local (today)
+  seat: 16C
+  attributes: aisle seat in the middle of the plane.
+"""
